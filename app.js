4cd4fcc5c1d15906e5345b3988606f110ddb8e72
mj <xumj8955@126.com>
2017-04-19 14:46:11 +0800

    微信测试

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ const bodyparser = require('koa-bodyparser')();
 const logger = require('koa-logger');
 
 const index = require('./routes/index');
-const wechat = require('./routes/wechat');
-
+const wechat = require('co-wechat');
 // error handler
 onerror(app);
 
@@ -30,8 +29,55 @@ app.use(async (ctx, next) => {
   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
 });
 
+var config = {
+  token: 'xumj8955',
+  appid: 'wxf7420a1829a1e158',
+  appSecret: 'ebec8b16c12162f369bb2440bf774f26',
+  encodingAESKey: 'hMlbEtcang9FqJu2mnLfEFk5Vv89eEMJSINHk3RpFfA',
+  checkSignature: true // 可选，默认为true。由于微信公众平台接口调试工具在明文模式下不发送签名，所以如要使用该测试工具，请将其设置为false
+};
+app.use(wechat(config).middleware(async (message) => {
+  // 微信输入信息就是这个 message
+  if (message.FromUserName === 'diaosi') {
+    // 回复屌丝(普通回复)
+    return 'hehe';
+  } else if (message.FromUserName === 'text') {
+    //你也可以这样回复text类型的信息
+    return {
+      content: 'text object',
+      type: 'text'
+    };
+  } else if (message.FromUserName === 'hehe') {
+    // 回复一段音乐
+    return {
+      type: "music",
+      content: {
+        title: "来段音乐吧",
+        description: "一无所有",
+        musicUrl: "http://mp3.com/xx.mp3",
+        hqMusicUrl: "http://mp3.com/xx.mp3"
+      }
+    };
+  } else if (message.FromUserName === 'kf') {
+    // 转发到客服接口
+    return {
+      type: "customerService",
+      kfAccount: "test1@test"
+    };
+  } else {
+    // 回复高富帅(图文回复)
+    return [
+      {
+        title: '你来我家接我吧',
+        description: '这是女神与高富帅之间的对话',
+        picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
+        url: 'http://nodeapi.cloudfoundry.com/'
+      }
+    ];
+  }
+}));
+
 // routes
 app.use(index.routes(), index.allowedMethods());
-app.use(wechat.routes(), wechat.allowedMethods());
 
 module.exports = app;
